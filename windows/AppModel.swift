3a0e55f6933eb78596ace325eac0d48c5b2c6596
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-07-07 08:04:58 +0100

    Initial Commit

---
+++ windows/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  windows
+//
+//  Created by Kuti Gbolahan on 07/07/2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ windowsTests/windowsTests.swift
@@ -0,0 +1,17 @@
+//
+//  windowsTests.swift
+//  windowsTests
+//
+//  Created by Kuti Gbolahan on 07/07/2025.
+//
+
+import Testing
+@testable import windows
+
+struct windowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
