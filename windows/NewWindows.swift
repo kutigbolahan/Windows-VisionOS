e6f708068dc713bd349385f032ad68a26a20126c
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-07-07 08:36:08 +0100

    Addded multiple windows with swuft ui

---
+++ windows/NewWindows.swift
@@ -0,0 +1,18 @@
+//
+//  NewWindows.swift
+//  windows
+//
+//  Created by Kuti Gbolahan on 07/07/2025.
+//
+
+import SwiftUI
+
+struct NewWindows: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewWindows()
+}
